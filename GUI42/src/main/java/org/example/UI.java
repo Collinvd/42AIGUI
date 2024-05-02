a72587a9a0769552503ddd7bef94f37e70d0b250
JayUndrScr <jayjadi1406@gmail.com>
2024-05-02 14:23:58 +0200

    Ex Machina

--- GUI42/src/main/java/org/example/UI.java
+++ GUI42/src/main/java/org/example/UI.java
@@ -4,37 +4,46 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+//Extending UIElement to gain access to components of the GUI(JFrame excluding)
+//Implements ActionListener to look for Inputs from any button in the GUI
 public class UI extends UIElement implements ActionListener {
-
+    //Variables
     JFrame window = new JFrame();
     UI(){
+        //window setup
         window.setTitle("42GUI");
         window.setSize(800,600);
+        window.setMinimumSize(new Dimension(800,600));
         window.setResizable(true);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setVisible(true);
-
-        Toolkit toolkit = Toolkit.getDefaultToolkit();
-        Dimension screenSize = toolkit.getScreenSize();
+        //Center window
         int x = (screenSize.width - window.getWidth()) / 2;
         int y = (screenSize.height - window.getHeight()) / 2;
-
         window.setLocation(x, y);
-
-        layoutPanelW = getLayoutPanel(150,0,darkGray, new BorderLayout());
+        //Main Panels
+        layoutPanelW = getLayoutPanel(180,0,darkGray, new BorderLayout());
         layoutPanelC = getLayoutPanel(100,100,lightGray,new BorderLayout());
+        //Sub Panel Center
         promptPanel = getLayoutPanel(100,100,gray,null);
+        //Sub Panel West
         newChatButtonPanel = getLayoutPanel(0,40, darkGray, null);
-        chatPanel = getLayoutPanel(0, 0, gray, new FlowLayout(FlowLayout.CENTER, 1,1));
-
-        newChatButton = getNewChatButton(1,40,gray);
+        chatPanel = getLayoutPanel(0, 0, darkGray, new FlowLayout(FlowLayout.CENTER, 1,1), white, 1,1,0,0);
+        settings = getLayoutPanel(0,100, darkGray, null );
+        //Sub Button West
+        newChatButton = getNewChatButton(1,40,darkGray);
         newChatButtonPanel.add(newChatButton, BorderLayout.CENTER);
+        //Adding sub panels to Main Panels
         layoutPanelW.add(newChatButtonPanel, BorderLayout.NORTH);
         layoutPanelW.add(chatPanel, BorderLayout.CENTER);
+        layoutPanelW.add(settings, BorderLayout.SOUTH);
         layoutPanelC.add(promptPanel, BorderLayout.SOUTH);
+        //Adding main panels to window
         window.add(layoutPanelC, BorderLayout.CENTER);
         window.add(layoutPanelW, BorderLayout.WEST);
+        //Window finalization
+        window.setIconImage(logo.getImage());
+        window.pack();
+        window.setVisible(true);
     }
     @Override
     public void actionPerformed(ActionEvent e) {
--- GUI42/src/main/java/org/example/UIElement.java
+++ GUI42/src/main/java/org/example/UIElement.java
@@ -3,39 +3,64 @@ package org.example;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
-
+//Sorry i forgor to put any explanation
 public class UIElement {
+    Toolkit toolkit = Toolkit.getDefaultToolkit();
+    Dimension screenSize = toolkit.getScreenSize();
     //Layout panels
     JPanel layoutPanelW = new JPanel();
     JPanel layoutPanelC = new JPanel();
     JPanel promptPanel = new JPanel();
     JPanel newChatButtonPanel = new JPanel();
     JPanel chatPanel = new JPanel();
+    JPanel settings = new JPanel();
     //Buttons
     JButton newChatButton = new JButton();
-
+    JButton sendButton = new JButton();
+    //TextField
+    JTextField promptBar = new JTextField();
+    //Images
+    ImageIcon logo = new ImageIcon("42Logo.png");
     //Colors
     Color darkGray = new Color(28,28,28);
     Color gray = new Color(35,35,35);
     Color lightGray = new Color(40,40,40);
     Color white = Color.WHITE;
 
+
     JPanel getLayoutPanel(int width, int height, Color color, LayoutManager layout){
         JPanel panel = new JPanel();
-
         panel.setPreferredSize(new Dimension(width,height));
         panel.setBackground(color);
         panel.setLayout(layout);
         return panel;
     }
+    JPanel getLayoutPanel(int width, int height, Color color, LayoutManager layout, Color borderColor, int top ,int bottom, int left, int right){
+        JPanel panel = new JPanel();
+        Border border = BorderFactory.createMatteBorder(top,left,bottom,right,borderColor);
+        panel.setPreferredSize(new Dimension(width,height));
+        panel.setBackground(color);
+        panel.setLayout(layout);
+        panel.setBorder(border);
+        return panel;
+    }
+    JTextField getPromptBar(){
+        JTextField textField = new JTextField();
+        return  textField;
+    }
     JButton getNewChatButton(int width, int height, Color color){
         JButton button = new JButton();
         button.setForeground(white);
-        button.setText("+New Chat");
-        button.setBounds(15,5,120,30);
+        button.setHorizontalAlignment(JButton.LEFT);
+        button.setText("+  New chat");
+        button.setBounds(5,5,170,30);
         button.setOpaque(true);
         button.setBackground(color);
         button.setFocusPainted(false);
+        return button;
+    }
+    JButton getSendButton(){
+        JButton button = new JButton();
         return button;
     }
 }
